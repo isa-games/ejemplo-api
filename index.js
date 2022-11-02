50a75a149819908d5adb92f6c00a69ac91868363
Alexander Toscano Ricardo <kikret@gmail.com>
2022-11-02 11:43:10 +0000

    Add server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi')
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res)=>{
+    res.send('Hola a todos')
+})
+
+
+app.listen(port, ()=>{
+    console.log('Servers tarted!')
+})
